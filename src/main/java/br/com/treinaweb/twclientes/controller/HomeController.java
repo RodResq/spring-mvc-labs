0d23e05b0e5227559a3f9b92d73ffb77cafe37b3
RodResq <rodrigoresque@yahoo.com.br>
2022-01-08 22:58:12 -0300

    rvw, requestparams

--- src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -41,9 +41,10 @@ public class HomeController {
     // }
 
     @GetMapping("/saudacao")
-    public ModelAndView saudacao(@RequestParam String nome) {
+    public ModelAndView saudacao(@RequestParam(required = false, defaultValue = "treinaWeb") String nome) {
 
         ModelAndView modelAndView = new ModelAndView();
+
         modelAndView.setViewName("saudacao");
         modelAndView.addObject("nome", nome);
 
